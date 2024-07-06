8a8b452ca420b6e1e0a9f378ec9169d1e780e569
UNacc Shubh <154262783+KratosHere@users.noreply.github.com>
2024-07-06 19:15:44 +0000

    More Commit

---
+++ 01_Basics_General/libraryFunctions.c
@@ -0,0 +1,26 @@
+/*
+
+%d => For Integers!
+%f => For Real Values!
+%c => For Characters!
+
+
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    int PrintInteger = 23;
+    float PrintReal = 42.14;
+    char PrintChar = 'c';
+
+    printf("Integer: %d \n" , PrintInteger);
+
+    printf("Real: %f \n" , PrintReal);
+    
+    printf("Character: %c \n" , PrintChar);
+
+    return 0;
+
+}
