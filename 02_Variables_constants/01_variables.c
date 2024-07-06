e00387b6887ca86a613f5ec74363206fbbcad074
UNacc Shubh <154262783+KratosHere@users.noreply.github.com>
2024-07-06 18:36:37 +0000

    Variables constants and all started

---
+++ 02_Variables_constants/01_variables.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+
+    int Integer_Variable;
+    float Float_Variable;
+
+    int RollNumber = 21;
+    float _PIE_ = 3.14;
+
+    printf("This is my Roll Number: %d \n" , RollNumber);
+    printf("I AM VALID VARIABLE PIE: %f \n" , _PIE_);
+
+    return 0;
+}
---
+++ 02_Variables_constants/02_constants.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    int Integer_Constant = -142;
+    float Real_Constant = -3.14;
+    char Character_Constant = 's'; // Note => Enclosed In Single Inverted Commas!! (For Character Constant)
+
+    printf("%d" , Integer_Constant);
+
+    return 0;
+}
+
