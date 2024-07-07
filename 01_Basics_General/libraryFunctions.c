87d86788f448846ef1d1cb2b6522803a376bb55e
UNacc Shubh <154262783+KratosHere@users.noreply.github.com>
2024-07-07 18:51:53 +0000

    Added Extra To Previous

--- 01_Basics_General/libraryFunctions.c
+++ 01_Basics_General/libraryFunctions.c
@@ -15,6 +15,12 @@ int main() {
     float PrintReal = 42.14;
     char PrintChar = 'c';
 
+    // int TempInt = 4.5; // Not Possible Because 4.5 is not an Integer
+    // float TempReal = 3; // Not Possible Because 3 is not a Real Value
+
+    int PrintThisValue = 501;
+    printf("This is a Value: %d %d \n" , PrintThisValue , PrintThisValue);
+
     printf("Integer: %d \n" , PrintInteger);
 
     printf("Real: %f \n" , PrintReal);
